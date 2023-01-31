6a3fd2dc1928b24d8917600d17f3ade4890ba5b1
OlesiaKryvosheieva <koi.23102001@gmail.com>
2023-01-31 11:12:57 +0100

    done fetch

---
+++ src/fetchCountries.js
@@ -0,0 +1,11 @@
+function fetchCountries(name) {
+  if ((name = '')) {
+    return;
+  }
+
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(res => res.json());
+}
+
+export {fetchCountries};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './css/styles.css';
-
+import {fetchCountries} from './fetchCountries.js';
+import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
+
+
+
+const nameOfCountry = document.querySelector('[id="search-box"]')
+
+nameOfCountry.addEventListener('input', debounce(searchInfoOfCountry, DEBOUNCE_DELAY))
+
+
+function searchInfoOfCountry() {
+    fetchCountries('nameOfCountry.value');
+    
+}
+
+// console.log(fetchCountries(name));
